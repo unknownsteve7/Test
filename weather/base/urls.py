0f715e22772ae0e11e249f81ce564af741e2f912
Yaswanth332 <yaswanthkrishnajonnalagadda@gmail.com>
2025-03-21 01:00:44 +0530

    login

--- weather/base/urls.py
+++ weather/base/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import *
 urlpatterns = [
-    path('',main,name='main')
+    path('',main,name='main'),
+    path('login',CustomLoginView.as_view(),'login')
 ]
